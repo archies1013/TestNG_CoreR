6be97ea1bb0ee9d3f8f34123bdd90cb4c55d566f
Archana Deulkar <88867952+sawantarchna@users.noreply.github.com>
2023-03-17 14:44:38 -0400

    First commit - added TC01 demo

---
+++ TestNG_RSA/src/main/java/TestNG_RSA/TestNG_RSA/App.java
@@ -0,0 +1,13 @@
+package TestNG_RSA.TestNG_RSA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TestNG_RSA/src/test/java/testNGBasics/TC01.java
@@ -0,0 +1,43 @@
+package testNGBasics;
+
+import org.testng.annotations.Test;
+
+public class TC01 {
+
+	//public static void main(String[] args) {
+		
+		//Java Compiler
+/**public static void main(String[] args){
+ * }
+ * = bec of this line of code your test will run on JavaCompiler 
+ * Now TestNG act as a JavaCompiler for you, so if you can incorporate testNG Framework in testNG library into your testcases 
+ * so you need not depends upon JavaCompile. TestNG itself act as a JavaCompiler and execute all your test cases.
+ * Without this above line you cannot run and compile any Java program, you need this main method in Java to execute
+ * But still you need Java in backend but for front end if you testNg you dont need Java for frontend
+ */
+		//TestNg
+		/**Create one method first that is=
+		 * @Test
+		 * public void demo(){
+		 * "write code"
+		 * }
+		 * This is above is method syntax of TestNG, which has a public is a access modifier and void is a return type
+		 * and it testNG need all test case execution in method.
+		 * @Test = is one of the annotation in testNG library which will recognize the method which immediately 
+		 * followed after this annotation as test case and it will run run the program even if you don't give the
+		 * public static void main(String[] args)
+		 * 
+		 */
+	//}
+	
+	@Test
+	public void demo()
+	{
+		System.out.println("Hello");
+	}
+	
+	/**TestNgxml file we need to run test cases in TestNg
+	 * 
+	 */
+	
+}
